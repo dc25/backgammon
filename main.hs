271e68e570a87e041b174b95bdfefc38acd78e01
Dave Compton <davecompton7@gmail.com>
2015-06-11 17:40:11 -0700

    isolated code to set checker position and class; added code to parse checker identity

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 import Haste.DOM
 import Haste.Prim
 import Control.Monad.IO.Class
+import Data.String
 
 -- javascript functionality
 foreign import ccall jsCreateElemNS :: JSString -> JSString -> IO Elem
@@ -24,42 +25,52 @@ leftmostPoint = 10
 pointGap = 15
 barGap = 5
 
+setCheckerPosition :: Elem -> Int -> Int -> IO ()
+setCheckerPosition circle pointIndex checkerIndex = do
+    setAttr circle "cx" (show $ xBase + leftDelta)
+    setAttr circle "cy" (show $ yBase + stackDelta)
+    where 
+      pointOffset | pointIndex < 12 = 11 - pointIndex -- from lower leftmost point
+                  | otherwise       = pointIndex - 12 -- from upper leftmost point
+
+      barGapUsed | pointOffset < 6 = 0                -- left side of board
+                 | otherwise       = barGap           -- right side of board
+
+      leftDelta = barGapUsed + pointOffset * pointGap -- distance in horizonatal "board units"
+
+      stackDirection | pointIndex < 12 = -1           -- stacking up
+                     | otherwise       = 1            -- stacking down
+
+      stackDelta = stackDirection*checkerIndex*2*checkerRadius -- distance in vertical "board units"
+
+      xBase = leftmostPoint                           -- horizontal position on board
+
+      yBase | pointIndex < 12 = firstLowerLevel       -- vertical position on board
+            | otherwise       = firstUpperLevel
+
+setCheckerClass :: Elem -> Color -> Int -> Int -> Color -> IO ()
+setCheckerClass circle usersColor pointIndex checkerIndex color = 
+    setAttr circle "class" (svgCheckerClass color usersColor pointIndex checkerIndex) 
+    where 
+      svgCheckerClass color usersColor pi ci = 
+          (if (usersColor == color ) then "draggable " else "") -- can only move your own checkers
+          ++ show color 
+          ++ " pi" ++ show pi -- point index
+          ++ " ci" ++ show ci -- checker index
+
 drawChecker :: Color -> Int -> Int -> Color -> IO ()
 drawChecker usersColor pointIndex checkerIndex color = do
     Just d <- elemById "board" 
     circle <- newElemNS "http://www.w3.org/2000/svg" "circle"
-    setAttr circle "cx" (show $ xBase + leftDelta)
-    setAttr circle "cy" (show $ yBase + stackDelta)
     setAttr circle "r" (show $ checkerRadius)
-    setAttr circle "class" (svgCheckerClass color usersColor pointIndex checkerIndex) 
+    setCheckerPosition circle pointIndex checkerIndex
+    setCheckerClass circle usersColor pointIndex checkerIndex color 
     if (usersColor  == color) then (setAttr circle "onmousedown" "selectElement(evt)") else return ()
     addChild circle d
-    where 
-      svgCheckerClass color usersColor pi ci = (if (usersColor == color ) then "draggable " else "") ++ show color ++ " pi" ++ show pi ++ " ci" ++ show ci
-
-      pointOffset | pointIndex < 12 = 11 - pointIndex
-                  | otherwise       = pointIndex - 12
-
-      barGapUsed | pointOffset < 6 = 0
-                 | otherwise       = barGap
-
-      leftDelta = barGapUsed + pointOffset * pointGap
-
-      stackDirection | pointIndex < 12 = -1
-                     | otherwise       = 1
-
-      stackDelta = stackDirection*checkerIndex*2*checkerRadius
-
-      xBase = leftmostPoint
-
-      yBase | pointIndex < 12 = firstLowerLevel
-            | otherwise       = firstUpperLevel
 
 drawPoint :: Color -> Int -> Point -> IO ()
-drawPoint usersColor pointIndex (Point color count) = 
-    let drawAtPoint = drawChecker usersColor pointIndex
-        uncurryDrawAtPoint = uncurry drawAtPoint
-    in sequence_ $ map uncurryDrawAtPoint (zip [0..] $ replicate count color) 
+drawPoint usersColor pointIndex (Point color count) =
+    sequence_ $ map (uncurry $ drawChecker usersColor pointIndex) (zip [0..] $ replicate count color) 
 
 drawGame :: Game -> IO ()
 drawGame (Game points usersColor _) = sequence_ $ map (uncurry (drawPoint usersColor)) $ zip [0..] points
@@ -101,13 +112,21 @@ newGame :: Game
 newGame = Game gameStart White White
 
 dropCheckerCallback :: JSString -> Int -> Int -> IO ()
-dropCheckerCallback className x y = consoleLog_ffi  $ toJSStr logStr
-    where logStr = fromJSStr className ++ " " ++ show x  ++ " " ++ show y
+dropCheckerCallback className x y = do
+    let logStr = fromJSStr className ++ " " ++ show x  ++ " " ++ show y
+    consoleLog_ffi  $ toJSStr logStr
+    let classes = words $ fromJSStr className
+        piString = drop 2 $ classes !! 2    -- "piXX"
+        ciString = drop 2 $ classes !! 3  -- "ciXX"
+        pointIndex = read piString :: Int
+        checkerIndex = read ciString :: Int
+        logStr2 = show pointIndex  ++ " " ++ show checkerIndex
+    consoleLog_ffi  $ toJSStr logStr2
+
 
 main :: IO ()
 main = let gameInPlay = newGame
        in do drawGame gameInPlay
              setDropCheckerCallback_ffi $ toPtr dropCheckerCallback
-             -- enableSelection gameInPlay 
              -- setCallbacks gameInPlay 
 
