4bbdde741811a2e0d0f2f41bb7b59cf0680de614
Dave Compton <davecompton7@gmail.com>
2015-07-02 21:36:34 -0700

    continuing to migrate towards functional bootstrap ui.

--- doConnect.js
+++ doConnect.js
@@ -1,7 +1,11 @@
 /// <reference path="webrtc.d.ts" />
 var sharedKeyElement = document.getElementById('sharedKey');
 var chatInputElement = document.getElementById('chatInput');
+var joinGameInputElement = document.getElementById('joinGame');
 var dc;
+function initConnection() {
+    dc = new DataConnection(sharedKeyElement.value, handleMessage);
+}
 // This is called on an incoming message from our peer
 // You probably want to overwrite this to do something more useful!
 function handleMessage(event) {
@@ -11,11 +15,19 @@ function handleMessage(event) {
 }
 sharedKeyElement.onkeydown = function (e) {
     if (e.keyCode == 13) {
-        dc = new DataConnection(sharedKeyElement.value, handleMessage);
+        initConnection();
+        return false; // don't do default action (refresh page)
     }
+    return true; // do default action (display char)
 };
 chatInputElement.onkeydown = function (e) {
     if (e.keyCode == 13) {
         dc.send(chatInputElement.value);
+        return false; // don't do default action (refresh page)
     }
+    return true; // do default action (display char)
+};
+joinGameInputElement.onclick = function (e) {
+    initConnection();
+    return false;
 };
--- doConnect.ts
+++ doConnect.ts
@@ -2,7 +2,12 @@
 
 var sharedKeyElement = <HTMLInputElement>document.getElementById('sharedKey');
 var chatInputElement = <HTMLInputElement>document.getElementById('chatInput');
+var joinGameInputElement = <HTMLInputElement>document.getElementById('joinGame');
 var dc:DataConnection;
+
+function initConnection () {
+    dc = new DataConnection(sharedKeyElement.value, handleMessage);
+}
 
 // This is called on an incoming message from our peer
 // You probably want to overwrite this to do something more useful!
@@ -14,12 +19,21 @@ function handleMessage(event) {
 
 sharedKeyElement.onkeydown = function( e ) {
     if (e.keyCode == 13) {
-        dc = new DataConnection(sharedKeyElement.value, handleMessage);
+        initConnection();
+        return false; // don't do default action (refresh page)
     }
+    return true; // do default action (display char)
 }
 
 chatInputElement.onkeydown = function( e ) {
     if (e.keyCode == 13) {
         dc.send(chatInputElement.value);
+        return false; // don't do default action (refresh page)
     }
+    return true; // do default action (display char)
+}
+
+joinGameInputElement.onclick = function( e ) {
+    initConnection();
+    return false; 
 }
